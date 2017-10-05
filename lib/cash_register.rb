2169353ebf14a2fee5dc8ed972a11d821f1baf43
Juliana Fogg <julianafogg@gmail.com>
2017-10-05 21:53:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,12 +19,11 @@
   end
 
   def apply_discount
-    #binding.pry
-    if !discount
-      "There is no discount to apply."
+      if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
     else
-      @total = @total - (@total * discount/100)
-      "After the discount, the total comes to $#{@total}."
+      "There is no discount to apply."
     end
   end
 
